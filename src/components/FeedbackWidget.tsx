abd927358860bc770f32a816ab88d835438d6963
SuryaNaikoti <suryanaikoti@gmail.com>
2026-08-07 06:29:40 +0530

    fix(mobile): Hide green floating feedback helper button on mobile viewports

--- src/components/FeedbackWidget.tsx
+++ src/components/FeedbackWidget.tsx
@@ -38,7 +38,7 @@ export const FeedbackWidget: React.FC = () => {
   };
 
   return (
-    <div className="fixed bottom-6 right-6 z-50 font-sans">
+    <div className="hidden md:block fixed bottom-6 right-6 z-50 font-sans">
       {!isOpen ? (
         <button
           onClick={() => { setIsOpen(true); setSubmitted(false); }}
